0fa2f7572696627fad414d5e63b5aa92259ce8d1
mkhs <mkh_sharif@aol.com>
2017-01-27 00:29:50 -0500

    question 1: fifth commit

--- fp.rkt
+++ fp.rkt
@@ -1,13 +1,15 @@
 (define (reverse-general L)
-  (cond ((null? L)'())
-        ((list? L)
-         (append (reverse-general (cdr L))
-                 )
-         )
+  (cond ((null? L)'()) ;if L is null return empty list
+       ((list? L) ;check if L is a list
+        (append (reverse-general (cdr L))
+                (list (reverse-general (car L))) 
+         );take very last value of list to append, then append the new last item 
         )
+       (else (not (list? L)) L); return L if one last item left in list
+  )
  )
 
 
 
-  
+
   
